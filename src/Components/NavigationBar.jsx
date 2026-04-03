6d21e2dc5b9c802be0d7fc4ad78be67950406c99
tonyopuodho <tonyopuodho@gmail.com>
2026-04-03 15:04:29 +0300

    mobile navigation

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
-import { FaBars } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function NavigationBar() {
+  const [isOpen,setIsOpen] = useState(false)
   const navLinks = [
     {name:"Home",href:"#home"},
     {name:"About",href:"#about"},
@@ -10,9 +11,10 @@ function NavigationBar() {
     {name:"Testimonials",href:"#testimonials"},
     {name:"Barbers",href:"#barbers"},
     {name:"Contact",href:"#contact"}
-  ]
+  ]  
   return (
-    <div className='fixed top-0 w-full bg-black/40 h-[10vh] p-2 flex items-center'>
+    <>
+    <div className='fixed top-0 w-full bg-black/40 h-[10vh] p-2 flex items-center z-10'>
        
       <div className='flex justify-between md:w-[80%] items-center mx-auto w-full'>
         <h1 className='text-4xl md:text-4xl font-bold text-white items-center'>Barber<span className='
@@ -27,12 +29,28 @@ function NavigationBar() {
           }
           <button className='bg-white py-2 px-4 font-bold text-red-800 cursor-pointer rounded-md text-sm'>Signup</button>
         </div>
-        {/* mobile navigation*/}
-        <div className='lg:hidden text-4xl font-bold text-white'>
-          <FaBars/>
+        {/* mobile button*/}
+        <div className='lg:hidden text-4xl font-bold text-white transition-all duration-300 ease-in'>
+          <div onClick={() => {setIsOpen(!isOpen)}}>
+            {
+               isOpen ? <FaTimes/> : <FaBars/>
+            }
+          </div>
         </div>
       </div>
     </div>
+    {/* mobile navigation */}
+    <div className={`top-23 md:hidden w-full h-[60vh] bg-white flex flex-col transition-all duration-300 ease-in absolute  ${isOpen ? 'left-0' : 'left-[-100%]'}`}>
+      <div className='flex flex-col gap-4 mt-20 ml-2 text-xl text-gray-800 font-bold'>
+        {
+          navLinks.map((item) => (
+            <a href={item.href}>{item.name}</a>
+          ))
+        }
+      </div>
+      <button className='bg-red-400 text-white mt-5 py-2 w-[80%] rounded-md ml-2 text-xl'>signup</button>
+    </div>
+  </>
   )
 }
 
